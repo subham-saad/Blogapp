f1ffb8ae752c78f38da71f3bb8f058643a48d4f5
Shubhm S Sadh <77003206+subham-saad@users.noreply.github.com>
2025-08-01 14:25:19 +0300

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "blogapp-nbqg.vercel.app",
+      "/api": "http://localhost:8000/",
     }  
   } ,
   plugins: [react()],
